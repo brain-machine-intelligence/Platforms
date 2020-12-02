7368a75320a5fa20cf0819c4603ffe5b9b84513e
brain-machine-intelligence <31230723+brain-machine-intelligence@users.noreply.github.com>
2020-12-02 11:59:54 +0900

    Upload recognition memory
    Upload recognition memory

---
+++ Recognition_Memory/Assets/ArmMove.cs
@@ -0,0 +1,206 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ArmMove : MonoBehaviour
+{
+    public GameObject joint1, joint2, joint3, joint4; // 로봇 팔 관절
+
+    public GameObject Object1, Object2, Object3, Object4, Object5, Object6, Object7; // 테이블 위에 물체
+
+    public GameObject TargetObject, GrabPoint; // TargetObject는 로봇 손과 가장 가까운 물체, GrabPoint는 잡힌 물체가 붙어있을 포인트
+
+    public bool IsGrab; // 물체를 잡았는지 확인하는 값
+
+    public int ArmVelocity; // 로봇 움직이는 속도
+
+    public float AttractSpeed; // 물체를 잡았을때 물체가 따라가는 속도
+
+    public float GrabDistance; // 물체 잡기를 허용하는 거리
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        IsGrab = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inputKey(); // 조작 키에 대한 함수
+        TargetSet(); // 로봇 손으로 부터 가장 가까운 물체를 계산하고 지정하는 함수
+        GrabObject(); // 물체를 잡는 함수
+        if (Input.GetKeyDown(KeyCode.R)) // 재시작
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+
+    void TargetSet() // 로봇 손으로부터 가장 가까운 물체를 계산하고 지정하는 함수
+    {
+        float distanceToCloestItem = Mathf.Infinity;
+        GameObject closestItem = null;
+        GameObject[] allItems = GameObject.FindGameObjectsWithTag("Item");
+
+        foreach (GameObject currentItem in allItems)
+        {
+            float distanceToItem = (currentItem.transform.position - GrabPoint.transform.position).sqrMagnitude;
+            if(distanceToItem < distanceToCloestItem)
+            {
+                distanceToCloestItem = distanceToItem;
+                closestItem = currentItem;
+            }
+        }
+        Debug.DrawLine(GrabPoint.transform.position, closestItem.transform.position);
+        if(IsGrab == false)
+        {
+            TargetObject = closestItem;
+        }        
+    }
+
+    void GrabObject() // 물체를 잡는 함수
+    {
+
+        if (Vector3.Distance(GrabPoint.transform.position, TargetObject.transform.position) < GrabDistance)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                IsGrab = !IsGrab;
+            }
+            if (IsGrab == true)
+            {
+                TargetObject.transform.position = Vector3.MoveTowards(TargetObject.transform.position, GrabPoint.transform.position, AttractSpeed * Time.deltaTime);
+                TargetObject.GetComponent<Rigidbody>().useGravity = false;
+                TargetObject.GetComponent<Rigidbody>().freezeRotation = true;
+                TargetObject.transform.rotation = GrabPoint.transform.rotation;
+            }
+            if (IsGrab == false)
+             {
+                TargetObject.GetComponent<Rigidbody>().useGravity = true;
+                TargetObject.GetComponent<Rigidbody>().freezeRotation = false;
+            }
+            
+        }
+       
+    }
+
+    void inputKey() // 조작 키에 대한 함수
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            HingeJoint hinge = joint1.GetComponent<HingeJoint>();
+            JointMotor motor = hinge.motor;
+            motor.targetVelocity = -ArmVelocity;
+            hinge.motor = motor;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            HingeJoint hinge = joint1.GetComponent<HingeJoint>();
+            JointMotor motor = hinge.motor;
+            motor.targetVelocity = ArmVelocity;
+            hinge.motor = motor;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            HingeJoint hinge = joint1.GetComponent<HingeJoint>();
+            JointMotor motor = hinge.motor;
+            motor.targetVelocity = 0;
+            hinge.motor = motor;
+        }
+        if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            HingeJoint hinge = joint1.GetComponent<HingeJoint>();
+            JointMotor motor = hinge.motor;
+            motor.targetVelocity = 0;
+            hinge.motor = motor;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            HingeJoint hinge = joint2.GetComponent<HingeJoint>();
+            JointMotor motor = hinge.motor;
+            motor.targetVelocity = -ArmVelocity;
+            hinge.motor = motor;
+        }
+        if ((Input.GetKey(KeyCode.Z)))
+        {
+            HingeJoint hinge = joint2.GetComponent<HingeJoint>();
+            JointMotor motor = hinge.motor;
+            motor.targetVelocity = ArmVelocity;
+            hinge.motor = motor;
+        }
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            HingeJoint hinge = joint2.GetComponent<HingeJoint>();
+            JointMotor motor = hinge.motor;
+            motor.targetVelocity =0;
+            hinge.motor = motor;
+        }
+        if ((Input.GetKeyUp(KeyCode.Z)))
+        {
+            HingeJoint hinge = joint2.GetComponent<HingeJoint>();
+            JointMotor motor = hinge.motor;
+            motor.targetVelocity =0;
+            hinge.motor = motor;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            HingeJoint hinge = joint3.GetComponent<HingeJoint>();
+            JointMotor motor = hinge.motor;
+            motor.targetVelocity = ArmVelocity;
+            hinge.motor = motor;
+        }
+        if ((Input.GetKey(KeyCode.X)))
+        {
+            HingeJoint hinge = joint3.GetComponent<HingeJoint>();
+            JointMotor motor = hinge.motor;
+            motor.targetVelocity = -ArmVelocity;
+            hinge.motor = motor;
+        }
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            HingeJoint hinge = joint3.GetComponent<HingeJoint>();
+            JointMotor motor = hinge.motor;
+            motor.targetVelocity = 0;
+            hinge.motor = motor;
+        }
+        if ((Input.GetKeyUp(KeyCode.X)))
+        {
+            HingeJoint hinge = joint3.GetComponent<HingeJoint>();
+            JointMotor motor = hinge.motor;
+            motor.targetVelocity = 0;
+            hinge.motor = motor;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            HingeJoint hinge = joint4.GetComponent<HingeJoint>();
+            JointMotor motor = hinge.motor;
+            motor.targetVelocity = -ArmVelocity;
+            hinge.motor = motor;
+        }
+        if ((Input.GetKey(KeyCode.C)))
+        {
+            HingeJoint hinge = joint4.GetComponent<HingeJoint>();
+            JointMotor motor = hinge.motor;
+            motor.targetVelocity = ArmVelocity;
+            hinge.motor = motor;
+        }
+
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            HingeJoint hinge = joint4.GetComponent<HingeJoint>();
+            JointMotor motor = hinge.motor;
+            motor.targetVelocity = 0;
+            hinge.motor = motor;
+        }
+        if ((Input.GetKeyUp(KeyCode.C)))
+        {
+            HingeJoint hinge = joint4.GetComponent<HingeJoint>();
+            JointMotor motor = hinge.motor;
+            motor.targetVelocity = 0;
+            hinge.motor = motor;
+        }
+    }
+}
---
+++ Recognition_Memory/Assets/robotic_arm.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class robotic_arm : MonoBehaviour
+{
+
+
+    /*
+    private HingeJoint base_to_part0;
+    private HingeJoint part0_to_part1;
+    private HingeJoint part1_to_part2;
+    private HingeJoint part2_to_part3;
+    private HingeJoint part3_to_Rgrip;
+    private HingeJoint part3_to_Lgrip;
+    */
+
+    private List<HingeJoint> hinge_joints = new List<HingeJoint>();
+
+    private GameObject Parent_Object;
+
+    private GameObject Item_Object;
+
+    private List<string> joint_name_list = new List<string>();
+
+    private List<float> force_ratio = new List<float>();
+
+
+    // Use this for initialization
+    void Start()
+    {
+        string[] name_list = { "Base", "part1", "part2", "part3", "leftGrip", "RigthGrip" };
+        float[] force_rate_arr = {40,60,60,40,40,40};
+
+        Parent_Object = this.transform.parent.gameObject;
+        foreach (Transform child in transform)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                string object_name = name_list[i];
+                
+                if (child.gameObject.name.Equals(object_name))
+                {
+                    hinge_joints.Add(child.gameObject.GetComponent(typeof(HingeJoint)) as HingeJoint);
+                    joint_name_list.Add(object_name);
+                    break;
+                }
+            }
+        }
+
+        foreach(Transform child in transform.parent)
+        {
+            if (child.gameObject.name.Equals("Item"))
+            {
+                Item_Object = child.gameObject;
+            }
+        }
+
+        for (int i = 0; i < hinge_joints.Count; i++)
+        {
+            HingeJoint joint = hinge_joints[i];
+
+            if (joint_name_list[i] == "leftGrip")
+            {
+                JointMotor motor = joint.motor;
+
+                motor.force = -40;
+                motor.targetVelocity = 80;
+                motor.freeSpin = false;
+
+                joint.motor = motor;
+                joint.useMotor = true;
+                //motor.targetVelocity = 0;
+                //motor.force = 0;
+                //Parent_Object.GetComponent<SpawnObject>().ActivateEarthquakeAuto();
+            }
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        //Item_Object.GetComponent<SpawnObject>().ActivateEarthquakeAuto();
+        //FindObjectOfType<SpawnObject>().ActivateEarthquakeAuto();
+        //foreach (HingeJoint joint in hinge_joints)
+
+        /*
+        for(int i =0; i< hinge_joints.Count; i++)
+        {
+            HingeJoint joint = hinge_joints[i];
+
+            if(joint_name_list[i] == "Base")
+            { 
+                JointMotor motor = joint.motor;
+
+                motor.force = 20;
+                motor.targetVelocity = 80;
+                motor.freeSpin = false;
+
+                joint.motor = motor;
+                joint.useMotor = true;
+                //motor.targetVelocity = 0;
+                //motor.force = 0;
+                //Parent_Object.GetComponent<SpawnObject>().ActivateEarthquakeAuto();
+            }
+
+        }*/
+    }
+}
